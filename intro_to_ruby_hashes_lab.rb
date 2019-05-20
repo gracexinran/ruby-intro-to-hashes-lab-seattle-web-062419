61dc7d335dd2164e724f77526d38906d9b8e7d06
Xinran Che <gracexinran@gmail.com>
2019-05-20 22:03:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,6 +33,7 @@ def monopoly_with_third_tier
   monopoly[:railroads][:rent_in_dollars] = {}
   monopoly[:railroads][:rent_in_dollars][:one_piece_owned] = 25
   monopoly[:railroads][:rent_in_dollars][:two_piece_owned] = 50
+  monopoly[:railroads][:rent_in_dollars][:two_piece_owned]
   monopoly[:railroads][:rent_in_dollars][:three_piece_owned] = 100
   monopoly[:railroads][:rent_in_dollars][:four_piece_owned] = 200
   
